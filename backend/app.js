5aca5a85bcc7accdfca12d19fe91113c5c599fa3
rohan2734 <jagandevaki1@gmail.com>
2020-10-23 18:07:06 +0530

    removed the comments in app.js in the backend

--- backend/app.js
+++ backend/app.js
@@ -13,18 +13,6 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(cors());
-// app.use((req,res,next) => {
-//   res.setHeader('Access-Control-Allow-Origin','*');
-//   res.setHeader('Access-Control-Allow-Headers',
-//   'Origin,X-Requested-With,Content-Type,Accept,Authorization');
-
-//   res.setHeader('Access-Control-Allow-Methods',
-//   'GET,POST,PATCH,DELETE');
-
-//   next();
-// });
-
-
 
 //routes
 const authRoutes = require('./routes/authRoutes');

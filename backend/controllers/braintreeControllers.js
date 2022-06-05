d6c1c9656f1a581ab00dbb9a59f7e5c678ae3e7b
rohan2734 <jagandevaki1@gmail.com>
2022-06-05 12:02:47 +0530

    added backend

--- backend/controllers/braintreeControllers.js
+++ backend/controllers/braintreeControllers.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 
 const gateway = braintree.connect({
   environment: braintree.Environment.Sandbox,
-  merchantId:process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey:process.env.BRAINTREE_PRIVATE_KEY
+  merchantId:process.env.BRAINTREE_MERCHANT_ID || '95sqx7npwh6x5kdm',
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY || 'whd37fp5w2tt8kzp',
+  privateKey:process.env.BRAINTREE_PRIVATE_KEY || 'bb931a765f0887c09e1ffd7591e0af9c'
 })
 
 const generateToken = (req,res,next) => {
